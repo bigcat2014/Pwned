a90eee4db1dccf32537830d300ba1f250379a0fa
Logan Thomas <ltogan.2014@gmail.com>
2019-03-16 14:44:34 -0400

    Use regular expression instead of string manipulation

--- pwned.py
+++ pwned.py
@@ -3,6 +3,7 @@
 
 from getpass import getpass
 import hashlib
+import re
 import requests
 import sys
 
@@ -20,10 +21,12 @@ PREFIX_LEN = 5
 
 
 def process_response(response, hash_suffix):
+    # Use regular expressions to determine if the hashes match
+    pattern = re.compile(f'^{hash_suffix}', re.I)
     # Split response into lines
     for item in response.text.splitlines():
         # Check for the item with a matching hash suffix
-        if item.startswith(hash_suffix):
+        if pattern.match(item):
             return item
     # Item with matching hash suffix not found
     return None
@@ -33,7 +36,7 @@ def main():
     # Get the password from the user
     password = getpass('Password to check: ')
     # Hash the password with sha1
-    hash = hashlib.sha1(str.encode(password)).hexdigest().upper()
+    hash = hashlib.sha1(str.encode(password)).hexdigest()
 
     # Query the api for the hash prefix
     response = requests.get(f'{URL}{hash[:PREFIX_LEN]}')
@@ -47,9 +50,9 @@ def main():
     # Print information to user
     print(f'Hash = {hash}')
     if item:
-        print(f'Password has been leaked {item.split(":")[-1]} times according to the https://haveibeenpwned.com password API')
+        print(f'Password has been leaked {item.split(":")[-1]} times according to the https://haveibeenpwned.com/ Pwned Passwords API')
     else:
-        print(f'Password has not been leaked according to the https://haveibeenpwned.com password API')
+        print(f'Password has not been leaked according to the https://haveibeenpwned.com/ Pwned Passwords API')
 
 
 if __name__ == '__main__':
